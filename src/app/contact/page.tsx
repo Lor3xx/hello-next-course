c471e3f428ef0e373becd7f8cd38d0f37f6d7f24
Lorenzo Milani <lorenzo.milani3@studenti.unimi.it>
2026-03-17 10:46:37 +0100

    aggiunta env key

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,7 @@
 export default function Contact() {
   return (
     <div className="page">
-      <h1 className="title"> Contact </h1>
+      <h1 className="title"> Contact {process.env.NEXT_PUBLIC_CUSTOM_KEY} </h1>
     </div>
   );
 }
